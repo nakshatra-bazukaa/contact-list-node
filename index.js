efd81ab49716841f1e36e6a0bfdce55ed1e5bfa9
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2021-01-11 08:11:57 +0530

    Setup to render from ejs views completed

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 const port = 8000;
 
 const app = express();
+
+app.set('view engine", "ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    return res.render('home.ejs');
+})
 
 app.listen(port, err => {
     if(err) 
